c4735185f96b93290404c13ad8f52c347b524c45
ChangHwanLee <lch3491@gmail.com>
2020-11-09 17:37:00 +0900

    normal login

---
+++ pybo/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    author = models.ForeignKey(User,  on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    author = models.ForeignKey(User,  on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
+    
+    def _str__(self):
+        return self.content
+        
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+    
+    def _str__(self):
+        return self.content
